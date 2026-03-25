499b6d9b6441c57645c3706a3020dcb610e900fd
rodbren <rodbren@users.noreply.github.com>
2026-03-25 11:05:24 -0700

    Add AFT module files and Jinja templates

---
+++ terraform/modules/aft-account-request/versions.tf
@@ -0,0 +1,13 @@
+# Copyright Amazon.com, Inc. or its affiliates. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+terraform {
+  required_version = ">= 1.2.0, < 2.0.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 6.0.0, < 7.0.0"
+    }
+  }
+}
