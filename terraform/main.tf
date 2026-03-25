cc3d698511d43eb32fd723a7e62072de5f711a5e
rodbren <rodbren@users.noreply.github.com>
2026-03-25 10:46:59 -0700

    Request workload-1 account

---
+++ terraform/main.tf
@@ -0,0 +1,25 @@
+module "workload1" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "rodogrc+aftworkload1@amazon.com"
+    AccountName               = "workload-1"
+    ManagedOrganizationalUnit = "Sandbox"
+    SSOUserEmail              = "rodogrc+aftworkload1@amazon.com"
+    SSOUserFirstName          = "Workload1"
+    SSOUserLastName           = "aft"
+  }
+
+  account_tags = {
+    Environment = "sandbox"
+    ManagedBy   = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Platform Team"
+    change_reason       = "Initial account provisioning via AFT"
+  }
+
+  custom_fields          = {}
+  account_customizations_name = "sandbox"
+}
